104be007d1a9679253ac61f7f10a4e7d3b77490f
Frederic Dubuis <dubuisf00@gmail.com>
2022-08-31 19:12:25 +0000

    first commit

---
+++ script/main.js
@@ -0,0 +1,6 @@
+requirejs.config({
+    baseUrl: "\\int.ec.gc.ca\shares\C\CTX-Profiles\redirected\dubuisf.ECQUEBEC\Documents\CPI projet/script",
+    paths: {
+    app: "\\int.ec.gc.ca\shares\C\CTX-Profiles\redirected\dubuisf.ECQUEBEC\Documents\CPI projet/script"
+    }
+    });
