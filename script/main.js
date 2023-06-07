9caa9b7e41f7e5caed869f4eaa1ca00f6ce970b8
0Fred0 <109358215+0Fred0@users.noreply.github.com>
2023-06-07 08:29:08 -0400

    Delete script directory

--- script/main.js
+++
@@ -1,6 +0,0 @@
-requirejs.config({
-    baseUrl: "\\int.ec.gc.ca\shares\C\CTX-Profiles\redirected\dubuisf.ECQUEBEC\Documents\CPI projet/script",
-    paths: {
-    app: "\\int.ec.gc.ca\shares\C\CTX-Profiles\redirected\dubuisf.ECQUEBEC\Documents\CPI projet/script"
-    }
-    });
